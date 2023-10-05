08eaacf52d1461d2f0c0743c796e986182e09cb5
Mehedi Hasan <couict6@gmail.com>
2023-10-05 11:00:10 +0600

    add

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ btn.addEventListener('click', () => {
     document.getElementById('number').innerHTML=num;
 
 });
+
+
+
+// const number = document.querySelector('.number');
+// const btn = document.querySelector('.generate');
+//
+// // WAY 1 //
+//
+// // const generateRandomNumber = () => {
+// //   const random = Math.floor(Math.random() * 1000)
+//
+// //   number.innerHTML = random;
+// // }
+// // btn.addEventListener('click', generateRandomNumber)
+//
+// // generateRandomNumber()
+//
+// // WAY 2
+//
+// btn.addEventListener('click', () => {
+//     const random = Math.floor(Math.random() * 1000)
+//
+//     number.innerHTML = random;
+// })
